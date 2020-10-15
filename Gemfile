9a78cd891f588ee9a28ea75fd087a805020c64b3
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-10-15 20:48:54 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ end
 
 # Windows ではタイムゾーン情報用の tzinfo-data gem を含める必要があります
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'bootstrap-sass', '3.4.1'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,51 +11,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
-end
-
-
-# def random_subdomain()
-#   ("a".."z").to_a.sample(8).join
-# end
-
-# def string_shuffle(s)
-#   "foobar".split("").shuffle.join
-#  end
-# person1={first:"e",last:"ff"}
-# person2={first:"ss",last:"w"}
-# person3={first:"q",last:"gg"}
-# params ={}
-# params[:father]=person1
-# params[:mother]=person2
-# params[:child] =person3
-# class Word
-#   def initialize
-#     @name="ddd"
-#   end
-#   def name
-#    p self
-#     puts  @name
-#   end
-#   def self.aa
-#     p self
-#     puts "ssssss"
-#   end
-# end
-# class Word              # WordクラスはStringクラスを継承する
-#    # 文字列が回文であればtrueを返す
-#    def initialize(name,age)
-#      @name=name
-#      @age = age
-#    end
-
-#     def palindrome?
-#       puts @name.reverse
-#    end
-#  end
-class String
-  def shuffle
-    split("").shuffle.join
-  end
 end
